8b8b19c3799f8bd5fdb9b68813ca18b48b5f435a
p0vned <strzebonsky@gmail.com>
2020-10-17 15:33:51 +0200

    Added basic logic to class, which obtain data from uri

--- PkCal/Tools/FileFromWebObtainer.cs
+++ PkCal/Tools/FileFromWebObtainer.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RestSharp;
+using System;
 
 namespace PkCal.Tools
 {
     public class FileFromWebObtainer
     {
+        private RestClient _client;
+        private Uri _uri;
 
+        public FileFromWebObtainer(Uri calendarEndpoint)
+        {
+            _uri = calendarEndpoint;
+            _client = new RestClient(_uri);
+        }
+
+        public void GetCalendarData()
+        {
+            var request = new RestRequest(Method.GET);
+            var response = _client.Execute(request);
+            var content = response.Content;
+        }
     }
 }
