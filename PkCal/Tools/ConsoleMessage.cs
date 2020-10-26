2b0b19220b6e5f27ff598f2dea61dde08d9f48e2
p0vned <strzebonsky@gmail.com>
2020-10-26 17:39:34 +0100

    Corrected typo

--- PkCal/Tools/ConsoleMessage.cs
+++ PkCal/Tools/ConsoleMessage.cs
@@ -15,20 +15,20 @@ namespace PkCal.Tools
 
         public static void PrintSuccessMessage(string message)
         {
-            PrintMessageWithChangedColour(message, ConsoleColor.Green);
+            PrintMessageWithChangedColor(message, ConsoleColor.Green);
         }
 
         public static void PrintWarningMessage(string message)
         {
-            PrintMessageWithChangedColour(message, ConsoleColor.Yellow);
+            PrintMessageWithChangedColor(message, ConsoleColor.Yellow);
         }
 
         public static void PrintErrorMessage(string message)
         {
-            PrintMessageWithChangedColour(message, ConsoleColor.Red);
+            PrintMessageWithChangedColor(message, ConsoleColor.Red);
         }
 
-        private static void PrintMessageWithChangedColour(string message, ConsoleColor color)
+        private static void PrintMessageWithChangedColor(string message, ConsoleColor color)
         {
             var currentForegroundColor = Console.ForegroundColor;
             
