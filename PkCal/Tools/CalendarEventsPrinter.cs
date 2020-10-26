c6d7485e3ea1c5332a994f806c9d7dab26483401
p0vned <strzebonsky@gmail.com>
2020-10-26 16:56:13 +0100

    Added class that prints events from calendar

---
+++ PkCal/Tools/CalendarEventsPrinter.cs
@@ -0,0 +1,31 @@
+﻿using Ical.Net.CalendarComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PkCal.Tools
+{
+    class CalendarEventsPrinter
+    {
+        private IEnumerable<CalendarEvent> _events;
+
+        public CalendarEventsPrinter(IEnumerable<CalendarEvent> events)
+        {
+            _events = events;
+        }
+
+        public bool PrintAllEvents()
+        {   
+            foreach (var calendarEvent in _events)
+            {
+                Console.WriteLine("======");
+                Console.WriteLine(string.Format("[KURS] {0}", calendarEvent.Categories.SingleOrDefault()));
+                Console.WriteLine(string.Format("[NAZWA WYDARZENIA] {0}", calendarEvent.Summary));
+                Console.WriteLine(string.Format("[OPIS] {0}", calendarEvent.Description));
+                Console.WriteLine(string.Format("[TERMIN DO] {0}", calendarEvent.DtEnd));
+            }
+
+            return _events.Count() != 0;
+        }
+    }
+}
